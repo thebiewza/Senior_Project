2ae8fe3b929dd3f2794d0eaa06985dd7a5dee1cf
thebiewza <ratchanontip@gmail.com>
2023-04-10 17:19:51 +0700

    progress3 10/4
    bowling + beatSaber

---
+++ Test_beatSaber_2/Assets/Script/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //public TMPro.TextMeshPro scoreLabel;
+    public TextMesh scoreLabel;
+    public int score = 0;
+    //public int hp = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        scoreLabel.text = "Score : " + score;
+        getScore();
+        //hpLabel.text = "Live : " + hp;
+
+        
+    }
+    public void getScore()
+    {
+        score = saberL.scoreL + saberR.scoreR;
+    }
+
+    //public void getDamage()
+    //{
+    //    hp = hp - 1;
+    //}
+}
